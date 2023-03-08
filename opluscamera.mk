ca916126fad5fe8e69beb1e95c7f3f3902b6d316
Pranav Vashi <neobuddy89@gmail.com>
2023-03-08 11:01:43 +0530

    Add google lens permission for oplus cam
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- opluscamera.mk
+++ opluscamera.mk
@@ -1,6 +1,7 @@
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/oplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml \
+    $(LOCAL_PATH)/configs/permissions/oplus_google_lens_config.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/oplus_google_lens_config.xml \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-oplus.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-oplus.xml \
     $(LOCAL_PATH)/configs/sysconfig/hiddenapi-package-oplus-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-oplus-whitelist.xml
 
@@ -15,8 +16,10 @@ PRODUCT_BOOT_JARS += \
 TARGET_USES_OPLUS_CAMERA := true
 
 # Properties
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.vendor.camera.privapp.list=com.oplus.camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.camera.privapp.list=com.oplus.camera \
+    ro.com.google.lens.oem_camera_package=com.oplus.camera \
+    ro.com.google.lens.oem_image_package=com.oneplus.gallery
     
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += \
